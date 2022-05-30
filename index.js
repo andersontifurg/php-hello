3138108033e07a283d1d39cb6ea091f221f7ad22
andersontifurg <106156775+andersontifurg@users.noreply.github.com>
2022-05-30 08:42:15 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+let app = express();
+
+app.use(express.static("."));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.php')
+})
+
+app.listen("8080", () => {
+    console.log("porta 8080")
+})
+
+function teste(){
+    console.log(x);
+
+}
